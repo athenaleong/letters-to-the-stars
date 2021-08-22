864c0e0f8e5be6e00db3bf7909ce8fbdf2e47ed9
Julia <julia.poo.poo.poo@gmail.com>
2021-08-22 15:46:22 +0800

    Removed duplicated external libraries

--- js/mouse.js
+++ js/mouse.js
@@ -1,3 +1,7 @@
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+
+export {MouseControl};
+
 /** 
 * Class that handles camera view based on mouse input
 * Camera z-coordinate has to be positive.
